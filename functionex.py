de19cb461687a9b7fc21450095815680bf57ee34
gokulselvam87 <gokul416442@gmail.com>
2025-10-06 18:38:21 +0530

    pyfunctions

---
+++ functionex.py
@@ -0,0 +1,24 @@
+def add():
+    print("Addiction:")
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print(a+b)
+def sub():
+    print("Subtraction:")
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a-b)
+add()
+sub()
+def multi():
+    print("Multiplication:")
+    a=int(input("enter a :"))
+    b=int(input("enter b:"))
+    print(a*b)
+multi()
+def div():
+    print("Division:")
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a/b)
+div()
